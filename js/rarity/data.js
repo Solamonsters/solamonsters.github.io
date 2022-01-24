d0eb8a7d097dc42fe19f226743c0d95c6bfb6f2c
zoupaul <paul.w.zou@gmail.com>
2022-01-24 17:07:35 -0500

    update

---
+++ js/rarity/data.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    "290.755188",
+        $("#table").DataTable({
+            scroller: true,
+            dom: "Bfrtip",
+            sScrollX: "100%",
+            scrollY: "70vh",
+            paging: true,
+            bPaginate: false,
+            bLengthChange: false,
+            deferRender: true,
+            sScrollXInner: "100%",
+            data: [
+
+            ],
+            columns: [
+                { title: "Rank" },
+                { title: "Name" },
+                { title: "Background" },
+                { title: "Skin" },
+                { title: "Headwear" },
+                { title: "Eyes" },
+                { title: "Mouth" },
+                { title: "Clothes" },
+                { title: "Chains" },
+                { title: "Piercings" },
+                { title: "Threat Level" },
+                { title: "View on ME" },
+            ],
+            buttons: ["csv", "json", "print"],
+            iDisplayLength: -1,
+        });
+});
